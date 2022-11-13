16b4446e41bfc8ad9cbecb8e8b4c25ae92349bc5
roman-supercode <roman.supercode@gmail.com>
2022-11-13 14:58:25 +0100

    complete

---
+++ assets/js/main.js
@@ -0,0 +1,71 @@
+// Es muss eine Funktion entwickelt werden, mit der man einfachen Text in Morsecode übersetzen kann.
+let morseAlphabet = [
+    { letter: "1", morseCode: ".----" },
+    { letter: "2", morseCode: "..---" },
+    { letter: "3", morseCode: "...--" },
+    { letter: "4", morseCode: "....-" },
+    { letter: "5", morseCode: "....." },
+    { letter: "6", morseCode: "-...." },
+    { letter: "7", morseCode: "--..." },
+    { letter: "8", morseCode: "---.." },
+    { letter: "9", morseCode: "----." },
+    { letter: "0", morseCode: "-----" },
+    { letter: " ", morseCode: "    " },
+    { letter: "A", morseCode: ".-" },
+    { letter: "B", morseCode: "-..." },
+    { letter: "C", morseCode: "-.-." },
+    { letter: "D", morseCode: "-.." },
+    { letter: "E", morseCode: "." },
+    { letter: "F", morseCode: "..-." },
+    { letter: "G", morseCode: "--." },
+    { letter: "H", morseCode: "...." },
+    { letter: "I", morseCode: ".." },
+    { letter: "J", morseCode: ".---" },
+    { letter: "K", morseCode: "-.-" },
+    { letter: "L", morseCode: ".-.." },
+    { letter: "M", morseCode: "--" },
+    { letter: "N", morseCode: "-." },
+    { letter: "O", morseCode: "---" },
+    { letter: "P", morseCode: ".--." },
+    { letter: "Q", morseCode: "--.-" },
+    { letter: "R", morseCode: ".-." },
+    { letter: "S", morseCode: "..." },
+    { letter: "T", morseCode: "-" },
+    { letter: "U", morseCode: "..-" },
+    { letter: "V", morseCode: "...-" },
+    { letter: "W", morseCode: ".--" },
+    { letter: "X", morseCode: "-..-" },
+    { letter: "Y", morseCode: "-.--" },
+    { letter: "Z", morseCode: "--.." }
+];
+
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    // get Input value and set to upper case
+    let userInput = (document.getElementById("userInput").value).toUpperCase();
+    // console.log(userInput);
+    const outputMorseCode = document.getElementById("output");
+
+    const outputArray = [];
+
+    // Clears the Input-field
+    document.getElementById("userInput").value = "";
+
+    // Loop for user-input
+    for (let character of userInput) {
+
+        // Loop for Array Object
+        morseAlphabet.forEach(function (item) {
+            if (character === item.letter) {
+                outputArray.push(item.morseCode);
+                // console.log(item.morseCode);
+            }
+        });
+
+    };
+    // console.log(outputArray.join(""));
+    outputMorseCode.textContent = outputArray.join("");
+})
+
